783eefd7885042b2b6cff320818108b4b79a494a
MohammadShakaroun <aucemohamadchakaroun@gmail.com>
2023-01-23 14:51:20 +0200

    Todo items with fields

--- my-app/auth.ts
+++ my-app/auth.ts
@@ -1,20 +1,3 @@
-// Welcome to some authentication for Keystone
-//
-// This is using @keystone-6/auth to add the following
-// - A sign-in page for your Admin UI
-// - A cookie-based stateless session strategy
-//    - Using a User email as the identifier
-//    - 30 day cookie expiration
-//
-// This file does not configure what Users can do, and the default for this starter
-// project is to allow anyone - logged-in or not - to do anything.
-//
-// If you want to prevent random people on the internet from accessing your data,
-// you can find out how by reading https://keystonejs.com/docs/guides/auth-and-access-control
-//
-// If you want to learn more about how our out-of-the-box authentication works, please
-// read https://keystonejs.com/docs/apis/auth#authentication-api
-
 import { randomBytes } from 'crypto';
 import { createAuth } from '@keystone-6/auth';
 
@@ -33,34 +16,26 @@ const { withAuth } = createAuth({
   listKey: 'User',
   identityField: 'email',
 
-  // this is a GraphQL query fragment for fetching what data will be attached to a context.session
-  //   this can be helpful for when you are writing your access control functions
-  //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
   sessionData: 'name createdAt',
   secretField: 'password',
 
-  // WARNING: remove initFirstItem functionality in production
-  //   see https://keystonejs.com/docs/config/auth#init-first-item for more
-  initFirstItem: {
-    // if there are no items in the database, by configuring this field
-    //   you are asking the Keystone AdminUI to create a new user
-    //   providing inputs for these fields
+  initFirstItem: { 
+
     fields: ['name', 'email', 'password'],
 
-    // it uses context.sudo() to do this, which bypasses any access control you might have
-    //   you shouldn't use this in production
   },
 });
 
 // statelessSessions uses cookies for session tracking
 //   these cookies have an expiry, in seconds
 //   we use an expiry of 30 days for this starter
-const sessionMaxAge = 60 * 60 * 24 * 30;
+const sessionMaxAge = 0;
 
 // you can find out more at https://keystonejs.com/docs/apis/session#session-api
 const session = statelessSessions({
   maxAge: sessionMaxAge,
   secret: sessionSecret!,
 });
+  
 
 export { withAuth, session };
--- my-app/schema.ts
+++ my-app/schema.ts
@@ -1,56 +1,32 @@
-// Welcome to your schema
-//   Schema driven development is Keystone's modus operandi
-//
-// This file is where we define the lists, fields and hooks for our data.
-// If you want to learn more about how lists are configured, please read
-// - https://keystonejs.com/docs/config/lists
-
 import { list } from '@keystone-6/core';
 import { allowAll } from '@keystone-6/core/access';
 
-// see https://keystonejs.com/docs/fields/overview for the full list of fields
-//   this is a few common fields for an example
 import {
   text,
   relationship,
   password,
   timestamp,
-  select,
+  select ,
 } from '@keystone-6/core/fields';
-
-// the document field is a more complicated field, so it has it's own package
 import { document } from '@keystone-6/fields-document';
-// if you want to make your own fields, see https://keystonejs.com/docs/guides/custom-fields
 
-// when using Typescript, you can refine your types to a stricter subset by importing
-// the generated types from '.keystone/types'
 import type { Lists } from '.keystone/types';
 
 export const lists: Lists = {
   User: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
+   
     access: allowAll,
 
     // this is the fields for our User list
     fields: {
-      // by adding isRequired, we enforce that every User should have a name
-      //   if no name is provided, an error will be displayed
       name: text({ validation: { isRequired: true } }),
 
       email: text({
         validation: { isRequired: true },
-        // by adding isIndexed: 'unique', we're saying that no user can have the same
-        // email as another user - this may or may not be a good idea for your project
         isIndexed: 'unique',
       }),
 
       password: password({ validation: { isRequired: true } }),
-
-      // we can use this field to see what Posts this User has authored
-      //   more on that in the Post list below
       posts: relationship({ ref: 'Post.author', many: true }),
 
       createdAt: timestamp({
@@ -112,7 +88,7 @@ export const lists: Lists = {
 
         // a Post can have many Tags, not just one
         many: true,
-
+                                                                                
         // this is some customisations for changing how this will look in the AdminUI
         ui: {
           displayMode: 'cards',
@@ -125,7 +101,6 @@ export const lists: Lists = {
       }),
     },
   }),
-
   // this last list is our Tag list, it only has a name field for now
   Tag: list({
     // WARNING
@@ -133,10 +108,9 @@ export const lists: Lists = {
     //   if you want to prevent random people on the internet from accessing your data,
     //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
     access: allowAll,
-
     // setting this to isHidden for the user interface prevents this list being visible in the Admin UI
     ui: {
-      isHidden: true,
+      isHidden: true,  
     },
 
     // this is the fields for our Tag list
